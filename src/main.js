4798326f85b2e7bcaabfc61dd65943692b42d6a6
markoleht <54833627+markoleht@users.noreply.github.com>
2020-04-07 13:25:25 +0300

    initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+    apiKey: "AIzaSyCddVNbSWS01Riy47REqSRlu0Lo4NgJtsg",
+    authDomain: "tartu-etk.firebaseapp.com",
+    databaseURL: "https://tartu-etk.firebaseio.com",
+    projectId: "tartu-etk",
+    storageBucket: "tartu-etk.appspot.com",
+    messagingSenderId: "491308083667",
+    appId: "1:491308083667:web:15c5b6d7c4bf2906152726",
+    measurementId: "G-E594XTR96E"
+
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
